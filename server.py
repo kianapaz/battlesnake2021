a08eec1cb4529731fadd08e53b5ad09f9a067e5c
kianapaz <kianapaz021@gmail.com>
2021-05-23 18:09:12 -0700

    cheking food

--- server.py
+++ server.py
@@ -106,10 +106,12 @@ class Battlesnake(object):
             if 'up' in possible_moves:
                 possible_moves.remove('up')
 
-        if second_body_part['x'] == head['x']+1 or second_body_part['x'] == head['x']-1 and second_body_part['y'] == head['y']+1 or second_body_part['y'] == head['y']-1:
+        if second_body_part['x'] == head['x']+1 or second_body_part['x'] == head['x']-1:
             print("hello")
             possible_moves = get_moves(possible_moves, head, second_body_part)
-
+        if second_body_part['y'] == head['y']+1 or second_body_part['y'] == head['y']-1
+            print("hello")
+            possible_moves = get_moves(possible_moves, head, second_body_part)
         if tail['x'] == head['x']+1 or tail['x'] == head['x']-1:
             print("ahhh")
             possible_moves = get_moves(possible_moves, head, tail)
